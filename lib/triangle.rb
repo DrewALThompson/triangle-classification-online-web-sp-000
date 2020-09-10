1cf3f53b21620318504cc7367272ef75b31607f6
Drew Thompson <drewallynthompson@gmail.com>
2020-09-10 02:26:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,12 +2,14 @@ class Triangle
   
   attr_accessor :length_one, :length_two, :length_three
   
-  def initialize(sides)
-    sides.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(:length_one, :length_two, :length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
   end
   
   def kind
-  
+    
   end
   
   class TriangleError < StandardError
