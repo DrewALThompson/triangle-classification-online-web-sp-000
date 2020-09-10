d331af2a7a7f653285018ae4bf0ee47a37483e87
Drew Thompson <drewallynthompson@gmail.com>
2020-09-10 03:12:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,11 +12,11 @@ class Triangle
     if ((@a <= 0)|| (@b <= 0) || (@c <= 0)) || ((@a + @b > @c) || (@a + @c > @b) || (@b + @c > @a))
       raise TriangleError
     else
-      if @a == @b && @b == @c 
+      if (@a == @b) && (@b == @c)
         :equilateral
-      elsif @a == @b || @b == @c || @a == @c 
+      elsif (@a == @b) || (@b == @c) || (@a == @c)
         :isosceles
-      else 
+      elsif (@a != @b) && (@b != @c) && (@a != @c)
         :scalene
       end
     end 
