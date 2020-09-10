587459b3d06394ee3641356dc68e59c9efc894ba
Drew Thompson <drewallynthompson@gmail.com>
2020-09-10 03:06:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,9 +11,14 @@ class Triangle
   def kind
     if (@a <= 0 || @b <= 0 || @c <= 0) || (@a + @b > @c || @a + @c > @b || @b + @c > @a)
       raise TriangleError
-    elsif @a == @b == @c 
+    elsif @a == @b && @b == @c 
       :equilateral
-    elsif 
+    elsif @a == @b || @b == @c || @a == @c 
+      :isosceles
+    else 
+      :scalene
+    end 
+  end
       
       
       
